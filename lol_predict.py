2da9b7ba8fda193b2417e4f62143dfd02b2630c9
erodium <liamhughes0@gmail.com>
2019-07-21 21:54:19 -0400

    messed up things

--- lol_predict.py
+++ lol_predict.py
@@ -1,8 +1,9 @@
-import json, requests, pprint
+import json, requests
 
 apiEndpoint = "https://api.lolesports.com/api/"
 
-LEAGUE_SLUGS = ["none", "all-star", "na-lcs", "eu-lcs", "na-cs", "eu-cs", "lck", "lpl-china", "lms", "worlds", "msi"]
+LEAGUE_SLUGS = [None, "all-star", "na-lcs", "eu-lcs", "na-cs", "eu-cs", "lck", "lpl-china", "lms", "worlds", "msi"]
+
 
 class Season():
 
@@ -15,19 +16,18 @@ class Season():
         self.games = tournament['gameIds']
         self.leagueId = tournament['league']
 
-
-#    def __repr__(self):
-#        return ("Season({},{},{})".format(
-#            self.title, self.description, self.id))
+    #    def __repr__(self):
+    #        return ("Season({},{},{})".format(
+    #            self.title, self.description, self.id))
 
     def __str__(self):
         return "title: {}\ndescription: {}\nid: {}".format(self.title, self.description, self.id)
 
     def getMatches(self):
-        return(self.games)
+        return (self.games)
 
     def getID(self):
-        return(self.id)
+        return (self.id)
 
 
 def getLeagueId(slug):
@@ -50,8 +50,8 @@ def getLeagues(_id=None, slug=None):
 
 
 def writeLeagueInfo(id):
-    if isinstance(leagueId, str) and leagueId in LEAGUE_SLUGS:
-        leagueId = LEAGUE_SLUGS.index(leagueId)
+    if isinstance(id, str) and id in LEAGUE_SLUGS:
+        id = getLeagueId(id)
     league_object = getLeagues(_id=id)
     with open("league" + str(id) + ".json", "w") as writefile:
         json.dump(league_object, writefile)
@@ -71,7 +71,7 @@ def getAllLeagueInfo():
 
 def getTournaments(leagueId):
     if isinstance(leagueId, str) and leagueId in LEAGUE_SLUGS:
-        leagueId = LEAGUE_SLUGS.index(leagueId)
+        leagueId = getLeagueId(leagueId)
     if isinstance(leagueId, int) and leagueId > 0 and leagueId < len(LEAGUE_SLUGS):
         r = requests.get(apiEndpoint + "v2/highlanderTournaments?league=" + str(leagueId), stream=True).json()
     else:
@@ -96,43 +96,86 @@ def getAllTournamentInfo():
         print("{} result {}".format(tnmt, result))
 
 
-
-
-
 def getTournamentInfo(id):
+    if isinstance(id, str) and id in LEAGUE_SLUGS:
+        id = LEAGUE_SLUGS.index(id)
     with open("league" + str(id) + "_tournaments.json", "r") as readfile:
         league_object = json.load(readfile)
-    #print("gti {}".format(type(league_object)))
-    #print(league_object.keys())
+    # print("gti {}".format(type(league_object)))
+    # print(league_object.keys())
     return league_object
 
 
 def getSeasonInfo(tourny, id):
-    #print("a {}".format(type(tourny)))
+    # print("a {}".format(type(tourny)))
     if isinstance(tourny, str):
-        #print("here")
+        # print("here")
         tourny = json.load(tourny)
     # print("b {}".format(type(tourny[id])))
     if isinstance(id, int) and id >= 0 and id < len(tourny['highlanderTournaments']):
         return tourny['highlanderTournaments'][id]
     else:
-        return("error in getSeasonInfo")
+        return ("error in getSeasonInfo")
 
+
+def getAllSeasons(tourny):
+    if isinstance(tourny, str):
+        tourny = json.load(tourny)
+    seasonList = tourny['highlanderTournaments']['brackets']
+    seasons = []
+    for bracketId, bracketDetails in seasonList.items():
+        season.append(bracketDetails)
+    return seasons
 
 def getMatchDetails(tournamentId, matchId):
-	r = requests.get(apiEndpoint+"v2/highlanderMatchDetails?tournamentId=" + str(tournamentId) + "&matchId=" + str(matchId)).json()
-	return r
+    endpoint = apiEndpoint + "v2/highlanderMatchDetails?tournamentId=" + str(tournamentId) + "&matchId=" + str(matchId)
+    print("getting {} match {} from {}".format(tournamentId, matchId, endpoint))
+    r = requests.get(endpoint).json()
+    return r
 
 
 # uncomment the below line to write all files anew.
 # getAllTournamentInfo()
-tourn = getTournamentInfo(2)
-lo = getSeasonInfo(tourn, 0)
+#for league in LEAGUE_SLUGS:
+#    tourn = getTournamentInfo(league)
+#    lo = getSeasonInfo(tourn, 0)
 
-s = Season(lo)
+#s = Season(lo)
 
 # print("lo {}".format(type(lo)))
-print(s)
-matches = s.getMatches()
-print(s.getID())
-print(getMatchDetails(s.getID(), matches[0]))
+#print(s)
+#matches = s.getMatches()
+#print(s.getID())
+#print(getMatchDetails(s.getID(), matches[0]))
+
+for league in LEAGUE_SLUGS:
+    print(league)
+    if league is None:
+        print("none")
+    else:
+        tourn = getTournamentInfo(league)
+        seasonsInfo = getAllSeasons(tourn)
+        for bracket in seasonsInfo:
+            print("{},{}".format(bracket['name']))
+            for matchId in season['gameIds']:
+                if index < 3:
+                    matchDetails = getMatchDetails(season['id'], matchId)
+                    print(matchDetails)
+                    index += 1
+                else: break
+
+"""
+
+
+tourn = getTournamentInfo("worlds")
+seasons = getAllSeasons(tourn)
+for season in seasons:
+    if season['id'] != "91be3d78-874a-44e0-943f-073d4c9d7bf6":
+        print("no")
+    else:
+        for matchId in season['gameIds']:
+            if matchId != "22851f97-7555-494d-b234-1e4bbeaf8dd5":
+                print("nope")
+            else:
+                matchDetails = getMatchDetails(season['id'], matchId)
+                print(matchDetails)
